87d674b9805c2606d1d14a1d8e7a9942a2f70a9e
Adam-Chmielinski <154561243+Adam-Chmielinski@users.noreply.github.com>
2025-11-14 09:53:24 +0000

    Update server.js

--- backend/API/server.js
+++ backend/API/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors'); // żeby frontend mógł robić requesty
+const cors = require('cors');
 
 const app = express();
 
-// Dodaj CORS dla frontendowej domeny
+
 app.use(cors({
   origin: 'https://twoj-frontend.netlify.app', // zmień na swój Netlify URL
 }));
