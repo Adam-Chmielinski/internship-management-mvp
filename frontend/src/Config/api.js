c32d9f9b756a22d792c4d2a8c875870fe092147a
Adam-Chmielinski <154561243+Adam-Chmielinski@users.noreply.github.com>
2025-11-17 13:36:54 +0000

    Remove unused API URL and login function
    Removed commented-out API URL and login function.

--- frontend/src/Config/api.js
+++ frontend/src/Config/api.js
@@ -1,15 +1,3 @@
 export const API_URL = 'https://172.24.0.67:3000';
 //export const API_URL = 'http://10.247.219.111:3000';
 export default API_URL;
-export async function login(email, password) {
-  const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email, password })
-  });
-
-  const data = await res.json();
-  return data;
-}
