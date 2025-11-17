e42a79c65ab6dd25b68ddbc8ec9f92c536e2bc9f
Adam-Chmielinski <154561243+Adam-Chmielinski@users.noreply.github.com>
2025-11-17 13:32:56 +0000

    Update api.js

--- frontend/src/Config/api.js
+++ frontend/src/Config/api.js
@@ -1,3 +1,7 @@
 export const API_URL = 'https://172.24.0.67:3000';
 //export const API_URL = 'http://10.247.219.111:3000';
 export default API_URL;
+export async function getPosts() {
+  const res = await fetch(`${process.env.REACT_APP_API_URL}/api/posts`);
+  return await res.json();
+}
