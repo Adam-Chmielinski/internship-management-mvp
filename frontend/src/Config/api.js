b2c45e30d4ef7d06c41a1001b63ddc89e0890bd2
Adam-Chmielinski <154561243+Adam-Chmielinski@users.noreply.github.com>
2025-11-17 13:35:12 +0000

    Add login function and update API URL

--- frontend/src/Config/api.js
+++ frontend/src/Config/api.js
@@ -1,7 +1,15 @@
 export const API_URL = 'https://172.24.0.67:3000';
 //export const API_URL = 'http://10.247.219.111:3000';
 export default API_URL;
-export async function getPosts() {
-  const res = await fetch(`${process.env.REACT_APP_API_URL}/api/posts`);
-  return await res.json();
+export async function login(email, password) {
+  const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  });
+
+  const data = await res.json();
+  return data;
 }
